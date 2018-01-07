05dfe9be016f85159e0afef098977596c2c4c6b2
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-06 22:05:38 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'help')) {
 	
-		 message.channel.send(message.author.toString() + `, need some help? :thinking: \n -a!channel\n -a!randomvideo\n -a!ping`);
+		 message.channel.send(message.author.toString() + `, need some help? :thinking: \n **-a!channel**\n **-a!randomvideo**\n **-a!ping**`);
 	}
 });
 client.on('message', message => {
