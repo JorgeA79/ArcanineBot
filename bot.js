b607ed08f4c85233729f3fcd078d7d4e7943311f
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-06 19:34:20 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready',() => {
 	//Edit after game so u can add wut game bot is playing between "" xd
-	   client.user.setPresence({game: {name: "Sel | a!help", type: 0}});
+	   client.user.setPresence({game: {name: "with Sel | a!help", type: 0}});
 });
 
 var prefix = "a!"
@@ -26,7 +26,7 @@ var randomVideo = [
 ]
 client.on('message', message => {
 	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'channel')) {
+	if (message.content.startsWith(prefix + 'ping')) {
 	
 		 message.channel.send(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``);
 	}
@@ -53,7 +53,13 @@ client.on('message', message => {
   message.channel.send({embed});
 	}
 });
-
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'randomvideo')) {
+	
+		 message.channel.send(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``);
+	}
+});
 //Important
 client.login(process.env.BOT_TOKEN);
 
