40bf042271771524881c1034e3fe8e9a9307de36
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-08 16:47:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -180,7 +180,7 @@ message.channel.createWebhook(name, avatar)
 
 client.on('message', message => {
 	if (message.author.bot){
-	if (message.content.startsWith('https')) {
+	if (message.content.startsWith('https://www.youtube')) {
 		var lastvideo = (message.content)
 	message.delete(1000);
 		 message.channel.send(`Everyone new video!` + lastvideo);
