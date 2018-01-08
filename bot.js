5255ab3323505be400f4b5fc6335a93bda5aa84e
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-01-07 22:47:35 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -156,9 +156,9 @@ message.channel.createWebhook(name, avatar)
 client.on('message', message => {
 	if (message.author.bot) 
 	if (message.content.startsWith(' https://www.youtube.com')) {
-	
+	 message.delete(1000);
 		 var messagexd = (message.content)
-		 message.delete(1000);
+		
 		 message.channel.send(`Everyone new video:\n `+ messagexd);
 
 	}
